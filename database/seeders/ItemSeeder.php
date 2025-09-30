a5fad0e48531726b1338c30eb9da7e4c48baf67e
sansan296 <sanshizu429@gmail.com>
2025-09-30 19:09:06 +0900

    Seederファイルの作成(エラーあり)

---
+++ database/seeders/ItemSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Item;
+use Carbon\Carbon;
+
+class ItemSeeder extends Seeder
+{
+  /**
+   * Run the database seeds.
+   */
+  public function run(): void
+  {
+    $now = Carbon::now();
+    $keyword = 'りんご';
+
+    for ($i = 0; $i < 20; $i++) {
+      Item::factory()->create([
+        'Item' => "{$keyword}",
+        'created_at' => $now->copy()->subSeconds(100 - $i),
+        'updated_at' => $now->copy()->subSeconds(100 - $i),
+      ]);
+    }
+
+    for ($i = 20; $i < 100; $i++) {
+      Item::factory()->create([
+        'created_at' => $now->copy()->subSeconds(100 - $i),
+        'updated_at' => $now->copy()->subSeconds(100 - $i),
+      ]);
+    }
+  }
+}
